ddcda84baeeb1cf14f86334e2b52260913c53ea8
IstiakAR <istiakrhyme840@gmail.com>
2024-12-12 22:49:23 +0600

    Updated fnkeys

--- .config/nvim/lua/config/fnkeys.lua
+++ .config/nvim/lua/config/fnkeys.lua
@@ -14,6 +14,11 @@ vim.api.nvim_set_keymap("n", "<F8>",
   ":w<CR>:lcd %:h<CR>:belowright 12split | terminal bash -c 'g++ % -o main && { ./main || echo -e \"\\033[31mRuntime error: $?\\033[0m\"; } || echo -e \"\\033[31mCompilation failed\\033[0m\" && g++ % -o main'<CR>i",
   { noremap = true, silent = true })
 
+--Run Python in a terminal with <F9>
+vim.api.nvim_set_keymap("n", "<F9>",
+  ":w<CR>:lcd %:h<CR>:belowright 12split | terminal python *.py <CR>i",
+  { noremap = true, silent = true })
+
 --Run Ruby in a terminal with <F10>
 vim.api.nvim_set_keymap("n", "<F10>",
   ":w<CR>:lcd %:h<CR>:belowright 12split | terminal ruby -e 'Dir[\"**/*.rb\"].each { |file| load file }' <CR>i",
@@ -24,7 +29,7 @@ vim.api.nvim_set_keymap("n", "<F11>",
   ":w<CR>:lcd %:h<CR>:belowright 15split | terminal javac % && java %:t:r || echo 'Compilation failed'<CR>i",
   { noremap = true, silent = true })
 
--- Run Java project in a terminal with <F12>
+-- Run JavaFX project in a terminal with <F12>
 vim.api.nvim_set_keymap("n", "<F12>",
   ":w<CR>:execute ':terminal (cd ~/Documents/JAVA_PROJECT && javac -d bin -cp \"lib/*\" $(find src -name \"*.java\") && java --module-path lib --add-modules javafx.controls,javafx.graphics,javafx.media -cp \"bin:lib/*\" Main)' | startinsert<CR>",
   { noremap = true, silent = true })
--- .gdbinit
+++ .gdbinit
@@ -1,3 +1,4 @@
 set pagination off
 set history save on
 set history expansion on
+source /usr/share/pwndbg/gdbinit.py
